7e22a8c160cd4bc926a926cd8dda0c6d83d10f6f
Yordy Almonte <yoalmonte.dev@outlook.com>
2024-04-14 13:18:28 -0400

    🥲 Seventh Commit

--- web-server/src/main.py
+++ web-server/src/main.py
@@ -1,4 +1,25 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+  return [1, 2, 3, 4, 5]
+
+@app.get('/contact', response_class = HTMLResponse)
+async def get_contact():
+  return '''
+    <html>
+      <head>
+        <title>Contact</title>
+      </head>
+      <body>
+        <h1>Contact</h1>
+        <p>Send us an email</p>
+    </html>
+  '''
 
 def run():
   store.get_data('https://api.escuelajs.co/api/v1/categories')
